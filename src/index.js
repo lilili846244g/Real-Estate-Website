e928a0eae2a86aa09d35af91cf5af41666a61911
ly li  <lilili846244@gmail.com>
2023-01-07 01:36:58 +0800

    i need sleep

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+//import HouseContextProvider
+import HouseContextProvider from "./components/HouseContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>
+  <HouseContextProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </HouseContextProvider>
 );
