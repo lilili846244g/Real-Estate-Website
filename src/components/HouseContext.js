79e1e272ed335381db587b04473d98938200ff6b
ly li  <lilili846244@gmail.com>
2023-01-10 00:39:40 +0800

    sleep

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -7,13 +7,63 @@ import { housesData } from "../data";
 export const HouseContext = createContext();
 
 const HouseContextProvider = ({ children }) => {
-  const [houses, setHoues] = useState(housesData);
+  const [houses, setHouses] = useState(housesData);
   const [country, setCountry] = useState("Location (any)");
   const [countries, setCountries] = useState([]);
-  const [property, setProperty] = useState("Property (any)");
+  const [property, setProperty] = useState("Property type(any)");
   const [properties, setProperties] = useState([]);
   const [price, setPrice] = useState("Price range (any)");
   const [loading, setLoading] = useState(false);
+
+  //  return all countries
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+    //remove duplicates
+    const uniqueCountries = ["Location (any)", ...new Set(allCountries)];
+    // set countries state
+    setCountries(uniqueCountries);
+  }, []);
+
+  /*======================properties========================*/
+  //  return all properties
+  useEffect(() => {
+    const allProperties = houses.map((house) => {
+      return house.type;
+    });
+    //remove duplicates
+    const uniqueProperties = ["Location (any)", ...new Set(allProperties)];
+    // set countries state
+    setProperties(uniqueProperties);
+  }, []);
+
+  const handleClick = () => {
+    //create a function that checks if the string includes "(any)"
+    const isDefault = (str) => {
+      return str.split("").includes("(any)");
+    };
+    //get first value of price and parse it to number
+    const minPrice = parseInt(price.split("")[0]);
+    //get second value of price which is the maximum price &  parse it to number
+    const maxPrice = parseInt(price.split("")[2]);
+
+    const newHouses = housesData.filter((houses) => {
+      const housePrice = parseInt(houses.price);
+
+      //if all values are selected
+
+      if (
+        house.country === country &&
+        house.type === property &&
+        housePrice >= minPrice &&
+        housePrice <= maxPrice
+      ) {
+        return house;
+      }
+    });
+    console.log(newHouses);
+  };
 
   return (
     <HouseContext.Provider
@@ -28,6 +78,7 @@ const HouseContextProvider = ({ children }) => {
         setPrice,
         houses,
         loading,
+        handleClick,
       }}
     >
       {children}
