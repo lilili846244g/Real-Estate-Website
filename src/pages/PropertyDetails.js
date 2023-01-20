16b408fbecf55b392d9f0aa9e0bb3c6c4be443c7
ly li  <lilili846244@gmail.com>
2023-01-20 20:03:37 +0800

    finish

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -61,8 +61,51 @@ const PropertyDetails = () => {
                 <div className="text-gray-500">{house.surface}</div>
               </div>
             </div>
-
             <div>{house.description}</div>
+          </div>
+          <div className="flex-1 bg-white w-full mb-8 border border-gray-300 rounded-lg px-6 py-8">
+            <div className="flex items-center gap-x-4 mb-8">
+              <div className="w-20 h-20 p-1 border border-gray-300 rounded-full">
+                <img src={house.agent.image} alt="" />
+              </div>
+              <div>
+                <div className="font-bold text-lg">{house.agent.name}</div>
+                <Link to="" className="text-violet-700 text-sm">
+                  View Listings
+                </Link>
+              </div>
+            </div>
+            {/*from*/}
+            <from className="flex flex-col gap-y-4">
+              <input
+                className="border border-gray-300 focus:border-violet-700 ouline-none rounded w-full px-4 h-14 text-sm"
+                type="text"
+                placeholder="Name*"
+              />
+              <input
+                className="border border-gray-300 focus:border-violet-700 ouline-none rounded w-full px-4 h-14 text-sm"
+                type="text"
+                placeholder="Email"
+              />
+              <input
+                className="border border-gray-300 focus:border-violet-700 ouline-none rounded w-full px-4 h-14 text-sm"
+                type="text"
+                placeholder="Phone"
+              />
+              <textarea
+                className="boder border-gray-300 focus:border-violet-700 ouline-none w-full resize-none rounded p-4 h-36 text-sm text-gray-400"
+                placeholder="Message*"
+                defaultValue="Hello, I am interested in [Modern apartment]"
+              ></textarea>
+              <div className="flex gap-x-2">
+                <button className="bg-violet-700 hover:bg-violet-800 text-white p-4 rounded text-sm w-full transition">
+                  Send message
+                </button>
+                <button className="border border-violet-700 text-violet-700 hover:border-violet-500  hover:text-violet-500  p-4  text-sm w-full rounded transition">
+                  Call
+                </button>
+              </div>
+            </from>
           </div>
         </div>
       </div>
